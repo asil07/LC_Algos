d857464410ee105af30ffb3b0d47fb62144bbc26
asilmuhammadiy <asilbekyuldashev07@gmail.com>
2022-10-19 13:47:56 +0900

    Life in weeks, days, months left

---
+++ basic_algos/life_left.py
@@ -0,0 +1,24 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+days = 365
+weeks = 52
+months = 12
+
+life = 90
+
+difference = life - int(age)
+calculate = f"You have {difference * 365} days, {difference * weeks} weeks, " \
+            f"{difference * months} months left till the age of 90"
+print(calculate)
+
+
+
+
+
+
+
+
